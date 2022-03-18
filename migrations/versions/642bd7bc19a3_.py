35d29c0991b0d83a9f1cc39ae96e5d102d288690
Code-rina <katerina.kreibich@gmail.com>
2022-03-18 08:42:48 -0700

    db fixed and signup working

--- migrations/versions/642bd7bc19a3_.py
+++ migrations/versions/642bd7bc19a3_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 71b3a624017f
+Revision ID: 642bd7bc19a3
 Revises: 
-Create Date: 2022-03-15 20:19:34.047103
+Create Date: 2022-03-18 08:31:36.629371
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '71b3a624017f'
+revision = '642bd7bc19a3'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,7 +23,7 @@ def upgrade():
     sa.Column('username', sa.String(length=35), nullable=False),
     sa.Column('fullname', sa.String(length=255), nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
-    sa.Column('profile_img', sa.String(length=255), nullable=False),
+    sa.Column('profile_img', sa.String(length=255), nullable=True),
     sa.Column('hashed_password', sa.String(length=255), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=True),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
