4de5e9c8f9c810ed076ddcff195646425d27047a
Code-rina <katerina.kreibich@gmail.com>
2022-03-21 09:44:29 -0700

    Mon am push

--- app/forms/add_comment_form.py
+++ app/forms/add_comment_form.py
@@ -8,5 +8,5 @@ from app.models import Comment
 class AddCommentForm(FlaskForm):
     user_id = IntegerField('user_id')
     post_id = IntegerField('post_id')
-    comment = TextAreaField('comment', validators=[DataRequired('Please provide a comment.'), Length(max=500, message="Maximum amount of characters for comment is 500.")])
+    comment = TextAreaField('comment', validators=[DataRequired('Please provide a comment.'), Length(min=1, max=500, message="Please provide a comment that has at least 1 character and not more than 500 characters.")])
     submit = SubmitField('submit')
