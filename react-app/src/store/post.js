e4e9f2977b88c031cbad533277ad862188f565a6
Code-rina <katerina.kreibich@gmail.com>
2022-04-13 12:55:55 -0700

    meta tags

--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -110,7 +110,7 @@ export const editOnePostThunk = ({ caption, id}) => async (dispatch) => {
     else if (response.status < 500) {
         const data = await response.json();
         if(data.errors) {
-            console.log("data::::",data)
+            // console.log("data::::",data)
             return data
         }
     }
